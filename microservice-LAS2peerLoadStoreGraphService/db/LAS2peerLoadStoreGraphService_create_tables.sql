b5eb9743bb37954216dab76d1851581a0501008c
CAEUser <CAEAddress@gmail.com>
2017-04-15 15:42:02 +0200

    Added microservice LAS2peerLoadStoreGraphService

---
+++ microservice-LAS2peerLoadStoreGraphService/db/LAS2peerLoadStoreGraphService_create_tables.sql
@@ -0,0 +1,19 @@
+--
+-- Database Schema:  commedit
+-- Automatically generated sql script for the service LAS2peerLoadStoreGraphService, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table graphs.
+--
+CREATE TABLE commedit.graphs (
+  links text ,
+  description varchar(250) ,
+  nodes text ,
+  graphId int ,
+CONSTRAINT graphId_PK PRIMARY KEY (graphId)
+ 
+);
+
+
+
