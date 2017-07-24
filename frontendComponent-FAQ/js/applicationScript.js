c328e744376aaa94f2dae1de6a14a9f2ff84db42
CAEUser <CAEAddress@gmail.com>
2017-07-24 12:29:40 +0200

    Added frontend component FAQ

---
+++ frontendComponent-FAQ/js/applicationScript.js
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2015 Advanced Community Information Systems (ACIS) Group, Chair
+ * of Computer Science 5 (Databases & Information Systems), RWTH Aachen
+ * University, Germany All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ * 
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ * 
+ * Neither the name of the ACIS Group nor the names of its contributors may be
+ * used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+var client;
+
+var init = function() {
+  
+  var iwcCallback = function(intent) {
+    // define your reactions on incoming iwc events here 
+    console.log(intent);
+
+  };
+
+  client = new Las2peerWidgetLibrary("$STEEN_URL$:$STEEN_PORT$", iwcCallback);
+
+  $('#loadFAQ').on('click', function() {
+    //start parameter initiation
+
+    //end parameter initiation
+    loadFAQ();
+  })
+  $('#submitFAQ').on('click', function() {
+    //start parameter initiation
+
+    //end parameter initiation
+    submitFAQ();
+  })
+
+
+}
+
+// loadFAQ
+var loadFAQ = function(){
+
+//start variable declaration
+   var loaded = null;
+
+//end variable declaration
+
+  client.sendRequest("GET", "/faq", "", "", {}, false,
+  function(data, type) {
+    data.forEach(function(entry) {
+        $("#entries").append("Q: " + entry.question + " " + "A:" + entry.answer);
+    }); 
+    console.log(data);
+  },
+  function(error) {
+    console.log(error);
+  });
+
+  //Additional own javascript
+ return loaded;
+}
+
+// submitFAQ
+var submitFAQ = function(){
+
+//start variable declaration
+   var created = null;
+
+//end variable declaration
+
+   var jsonContent = null; 
+ 
+jsonContent = $('#questionArea').val();
+   client.sendRequest("POST", "/faq", jsonContent, "text/plain", {}, false,
+  function(data, type) {
+    console.log(data);
+  },
+  function(error) {
+    console.log(error);
+  });
+
+  //Additional own javascript
+ return created;
+}
+
+
+$(document).ready(function() {
+  init();
+});
