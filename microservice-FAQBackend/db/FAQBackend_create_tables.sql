866c80c2c0cd5fbb6ff9d04fc3c88b918e35a056
CAEUser <CAEAddress@gmail.com>
2017-07-24 11:04:12 +0200

    Added microservice FAQBackend

---
+++ microservice-FAQBackend/db/FAQBackend_create_tables.sql
@@ -0,0 +1,19 @@
+--
+-- Database Schema:  faqs
+-- Automatically generated sql script for the service FAQBackend, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table faqs.
+--
+CREATE TABLE faqs.faqs (
+  category VARCHAR(255) ,
+  answer VARCHAR(255) ,
+  id ID ,
+  question VARCHAR(255) ,
+CONSTRAINT id_PK PRIMARY KEY (id)
+ 
+);
+
+
+
