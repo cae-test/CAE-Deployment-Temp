f6b86e0ea4a599c0cfe67ce6823f67fb379c751b
CAEUser <CAEAddress@gmail.com>
2019-07-17 19:31:59 +0200

    Added microservice ma-hossner-image-service

---
+++ microservice-ma-hossner-image-service/db/ma-hossner-image-service_create_tables.sql
@@ -0,0 +1,17 @@
+--
+-- Database Schema:  
+-- Automatically generated sql script for the service ma-hossner-image-service, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table Images.
+--
+CREATE TABLE .Images (
+  imageData TEXT ,
+  id INT ,
+CONSTRAINT id_PK PRIMARY KEY (id)
+ 
+);
+
+
+
